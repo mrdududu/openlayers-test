c6858ba15d84908bb426fc7b9640a905dcc9b874
Semen Ishinov <ectoplazm@gmail.com>
2024-10-21 15:35:39 +0300

    First commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// type Props = {}
+// type Emits = {
+//   (e: 'click', value: any): void
+// }
+
+// const props = defineProps<Props>()
+// const emits = defineEmits<Emits>()
+</script>
+<template>
+  <div>Index page</div>
+</template>
